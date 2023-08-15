733f519db6b0922458f8d1826909d80f93e15c29
Miracle Obinna <mmrealworld@yahoo.com>
2023-08-15 22:51:31 +0000

    largest_integer

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Find all multiples of 2 in a list."""
+
+    multiples = []
+
+    for k in range(len(my_list)):
+        if my_list[k] % 2 == 0:
+            multiples.append(True)
+
+        else:
+            multiples.append(False)
+
+    return multiples
