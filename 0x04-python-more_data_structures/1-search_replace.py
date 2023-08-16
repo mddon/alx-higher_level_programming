00e818e5120ea595108216b66146286dbc3767da
Miracle Obinna <mmrealworld@yahoo.com>
2023-08-16 19:03:55 +0000

    more_data_structure

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return new_list
+
+    updated_list = my_list[:]
+
+    for k in range(len(updated_list)):
+        if updated_list[k] == search:
+            updated_list[k] = replace
+
+    return updated_list
