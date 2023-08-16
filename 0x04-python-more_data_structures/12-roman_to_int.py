edb633a636392597cdea1001114d5e353e61676f
Miracle Obinna <mmrealworld@yahoo.com>
2023-08-16 21:46:23 +0000

    roman_string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 
+
 def roman_to_int(roman_string):
+    """Converts a roman numeral to an integer."""
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
 
-    roman_numerals = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
     }
-    total = 0
+    num = 0
 
-    for i in range(len(roman_string)):
-        if roman_numerals.get(roman_string[i], 0) == 0:
+    for k in range(len(roman_string)):
+        if roman_dict.get(roman_string[k], 0) == 0:
             return (0)
 
-        if (i != (len(roman_string) - 1) and
-                roman_numerals[roman_string[i]] < roman_numerals[roman_string[i + 1]]):
-            total += roman_numerals[roman_string[i]] * -1
+        if (k != (len(roman_string) - 1) and
+                roman_dict[roman_string[k]] < roman_dict[roman_string[k + 1]]):
+            num += roman_dict[roman_string[k]] * -1
 
         else:
-            total += roman_numerals[roman_string[i]]
-
-    return (total)
+            num += roman_dict[roman_string[k]]
+    return (num)
