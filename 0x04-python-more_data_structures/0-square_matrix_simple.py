38edacf907270644eb45ba9ee13c6e5fe87a6064
Miracle Obinna <mmrealworld@yahoo.com>
2023-08-16 18:49:23 +0000

    more_data_structure

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 
 def square_matrix_simple(matrix=[]):
 
-    return ([list(map(lambda k_element: k_element * k_element, row)) for row in matrix])
+    return [list(map(lambda k_element: k_element * k_element, row)) for row in matrix]
