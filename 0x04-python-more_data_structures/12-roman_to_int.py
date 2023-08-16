3e0d9ce8e3285e85947aec21cdfb5cef74859780
Miracle Obinna <mmrealworld@yahoo.com>
2023-08-16 21:42:37 +0000

    average

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or
             roman_string is None):
-        return 0
+        return (0)
 
     roman_numerals = {
         "I": 1,
@@ -18,11 +18,13 @@ def roman_to_int(roman_string):
 
     for i in range(len(roman_string)):
         if roman_numerals.get(roman_string[i], 0) == 0:
-            return 0
+            return (0)
 
         if (i != (len(roman_string) - 1) and
                 roman_numerals[roman_string[i]] < roman_numerals[roman_string[i + 1]]):
             total += roman_numerals[roman_string[i]] * -1
+
         else:
             total += roman_numerals[roman_string[i]]
-    return total
+
+    return (total)
