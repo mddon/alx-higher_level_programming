1d36122811f287b302bb9f84363de40759af52fe
Miracle Obinna <mmrealworld@yahoo.com>
2023-08-16 21:38:29 +0000

    average

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
 
+def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or
             roman_string is None):
-        return (0)
+        return 0
 
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+    roman_numerals = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
-    sum = 0
+    total = 0
 
-    for p in range(len(roman_string)):
-        if roman_numerals.get(roman_string[p], 0) == 0:
+    for i in range(len(roman_string)):
+        if roman_numerals.get(roman_string[i], 0) == 0:
             return 0
 
-        if (p != (len(roman_string) - 1) and
-                roman_numerals[roman_string[p]] < roman_numerals[roman_string[p + 1]]):
-            sum += roman_numerals[roman_string[p]] * -1
+        if (i != (len(roman_string) - 1) and
+                roman_numerals[roman_string[i]] < roman_numerals[roman_string[i + 1]]):
+            total += roman_numerals[roman_string[i]] * -1
         else:
-            sum += roman_numerals[roman_string[p]]
-    return sum
+            total += roman_numerals[roman_string[i]]
+    return total
