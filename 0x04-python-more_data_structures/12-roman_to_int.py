98dc64b89029e888d34a9c97f99a20fb7252f593
Miracle Obinna <mmrealworld@yahoo.com>
2023-08-16 21:35:32 +0000

    average

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
+
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
@@ -16,16 +14,15 @@ def roman_to_int(roman_string):
             "D": 500,
             "M": 1000
     }
-    num = 0
+    sum = 0
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+    for p in range(len(roman_string)):
+        if roman_numerals.get(roman_string[p], 0) == 0:
+            return 0
 
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-            num += roman_dict[roman_string[i]] * -1
-
+        if (p != (len(roman_string) - 1) and
+                roman_numerals[roman_string[p]] < roman_numerals[roman_string[p + 1]]):
+            sum += roman_numerals[roman_string[p]] * -1
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            sum += roman_numerals[roman_string[p]]
+    return sum
