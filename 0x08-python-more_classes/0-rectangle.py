267fcca61f02c309b52ce7163bd8d7d7953f2d89
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-06 18:11:41 +0000

    more_class&objects

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
-
+"""This script defines a Rectangle class."""
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Represents a rectangle."""
     pass
