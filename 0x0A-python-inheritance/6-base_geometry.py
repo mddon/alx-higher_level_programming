baa3e975886c1ddba7c34f16bf9b1f18933966bc
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-11 22:46:27 +0000

    P_Inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -9,7 +9,5 @@ class BaseGeometry:
         """Calculate the area of the geometry.
         Raises:
             Exception: This method is not implemented.
-        Note:
-            Subclasses should implement this method to calculate the area of the specific geometry.
         """
         raise Exception("area() is not implemented")
