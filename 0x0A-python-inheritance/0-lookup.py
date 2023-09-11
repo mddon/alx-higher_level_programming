19dffaae8c79e4d918187d4579887d83e51dc3cc
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-11 20:29:49 +0000

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,9 +3,6 @@
 
 def lookup(obj):
     """
-    Return a list of attributes and methods available for an object.
-    Args:
-        obj (object): The object to inspect.
     Returns:
         list: A list of available attributes and methods of the object.
     """
