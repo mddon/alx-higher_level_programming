2042573339c740dc0487a5cb9e8ab8d00ebf6376
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-11 22:13:28 +0000

    P_Inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """Script that defines a class and inherited class-checking function."""
 
 def is_kind_of_class(obj, a_class):
-i    """Check if an object is an instance or inherited instance of a class.
+    """Check if an object is an instance or inherited instance of a class.
 
     Args:
         obj (any): The object to check.
