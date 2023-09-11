3ae132ced86445101302d1d5e8a5edd3ff16a9d2
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-11 22:33:54 +0000

    P_Inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a function to check if an object is an inherited instance of a specific class."""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object is an inherited instance of a given class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+
+    Returns:
+        bool: True if obj is an inherited instance of a_class.
+        False otherwise.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
