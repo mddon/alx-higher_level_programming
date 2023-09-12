7602d0409fdd6e4f229c091129e418a58098596b
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-12 00:22:52 +0000

    P_Inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a class MyInt that inherits from int."""
+"""Script that defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
     """Invert int operators == and !=."""
 
     def __eq__(self, value):
-        """Override == operator with self.real."""
-        return self.real == value
+        """Override == opeartor with self.real."""
+        return self.real != value
 
     def __ne__(self, value):
         """Override != operator with self.real."""
-        return self.real != value
+        return self.real == value
