9a27db9ec112a1cda6fd8df9534573e5ee5d2428
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-12 20:55:40 +0000

    file i/o

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
+
