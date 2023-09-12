25cd4510bd9950949714181eb2cc5305a9fdae1a
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-12 21:57:35 +0000

    file i/o

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
-    '''Load existing items from a JSON file'''
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
     '''If the JSON file doesn't exist, initialize an empty list'''
