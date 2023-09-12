b837b6783fc60107fb9a0942c767f9647340d9e4
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-12 21:54:40 +0000

    file i/o

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Add command-line arguments to a Python list and save them to a JSON file."""
+import sys
+
+
+if __name__ == "__main__":
+    '''Necessary functions imported from external modules'''
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+    '''Load existing items from a JSON file'''
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+    '''If the JSON file doesn't exist, initialize an empty list'''
+        items = []
+    '''Extend the 'items' list with command-line arguments'''
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
