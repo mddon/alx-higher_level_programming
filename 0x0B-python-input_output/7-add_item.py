007895b38a03ec569b4aa0f87387af0915bf581c
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-12 22:01:56 +0000

    file i/o

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,15 +4,12 @@ import sys
 
 
 if __name__ == "__main__":
-    '''Necessary functions imported from external modules'''
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-    '''If the JSON file doesn't exist, initialize an empty list'''
         items = []
-    '''Extend the 'items' list with command-line arguments'''
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
