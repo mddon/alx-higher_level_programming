283b7cf29964fafdb15cc67dba2e60e566fe6f4d
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-13 00:59:52 +0000

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,11 +10,11 @@ def append_after(filename="", search_string="", new_string=""):
         search_string (str): The string to search for within the file.
         new_string (str): The string to insert.
     """
-    text = ""
+    content = ""
     with open(filename) as r:
         for line in r:
-            text += line
+            content += line
             if search_string in line:
-                text += new_string
+               content += new_string
     with open(filename, "w") as w:
         w.write(text)
