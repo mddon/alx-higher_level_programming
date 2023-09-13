e01d2c031694edd06add902f75808404bc15d084
Miracle Obinna <mmrealworld@yahoo.com>
2023-09-13 00:14:26 +0000

    file i/o

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,20 +4,16 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """Inserts text after each line containing a given string in a file.
-
     Args:
         filename (str): The name of the file to modify.
         search_string (str): The string to search for within each line.
         new_string (str): The string to insert after each line containing the search string.
     """
     modified_content = ""
-    
     with open(filename) as file_reader:
         for line in file_reader:
             modified_content += line
             if search_string in line:
                 modified_content += new_string
-                
     with open(filename, "w") as file_writer:
         file_writer.write(modified_content)
-
