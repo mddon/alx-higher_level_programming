e13c199efc149b4a4483cb1cc1d1e3b3b38f5e71
Miracle Obinna <mmrealworld@yahoo.com>
2023-10-17 15:16:46 -0700

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
--- Change the character set and collation for the entire 'hbtn_0c_0' database to UTF-8
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Change the character set and collation for the 'first_table' to UTF-8
-ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Converts the entire database hbtn_0c_0 to UTF8.
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
