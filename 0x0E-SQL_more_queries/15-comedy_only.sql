175806e4d996013123185836cd0ecba9c81b4cca
Miracle Obinna <mmrealworld@yahoo.com>
2023-10-19 09:25:53 -0700

    sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -6,4 +6,4 @@ FROM `tv_shows` AS t
 INNER JOIN `tv_show_genres` AS s ON t.`id` = s.`show_id`
 INNER JOIN `tv_genres` AS g ON g.`id` = s.`genre_id`
 WHERE g.`name` = "Comedy"
-ORDER BY t.`title` DESC;
+ORDER BY t.`title`;
