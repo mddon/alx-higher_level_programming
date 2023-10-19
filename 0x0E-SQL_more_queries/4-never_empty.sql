e29d46e1b883f85a138bbc1359357f6b1e0adddb
Miracle Obinna <mmrealworld@yahoo.com>
2023-10-19 05:05:34 -0700

    sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,4 @@
 --creates the table id_not_null on mysql server
 
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256));
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
 --creates the table unique_id on mysql server
 
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1,
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1,
     name VARCHAR(256),
     UNIQUE (id));
