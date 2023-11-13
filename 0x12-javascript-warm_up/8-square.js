dd8c962e0a0cf020f3f2086285d38a28035199c0
Miracle Obinna <mmrealworld@yahoo.com>
2023-11-13 21:24:51 +0000

    JS

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const len= Math.floor(Number(process.argv[2]));
-if (isNaN(len)) {
+if (isNaN(length)) {
   console.log('Missing size');
 } else {
-  for (let h = 0; h < len; h++) {
+  for (let h = 0; h < length; h++) {
     let row = '';
-    for (let w = 0; w < len; w++) row += 'X';
+    for (let w = 0; w < length; w++) row += 'X';
     console.log(row);
   }
 }
